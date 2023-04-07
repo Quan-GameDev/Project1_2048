6ca3e58bfc35e24c832e8cca8664956cc71915df
Tran Le Quan <75005806+Quan-GameDev@users.noreply.github.com>
2023-04-07 13:36:21 +0700

    To Game Over 1:32:43
    Finished:
    - Moving Tiles.
    - Animating Tiles.
    - Merging Tiles.

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -10,6 +10,7 @@ public class Tile : MonoBehaviour
 
     public int number { get; private set; }
     
+    public bool locked { get; set; }
 
     private Image background;
     private TextMeshProUGUI text;
@@ -25,7 +26,7 @@ public class Tile : MonoBehaviour
         this.state = state;
         this.number = number;
 
-        
+        background.color = state.backgroundColor;
         text.color = state.textColor;
         text.text = number.ToString();
     }
@@ -42,4 +43,48 @@ public class Tile : MonoBehaviour
         transform.position = cell.transform.position;
     }
 
+    public void MoveTo(TileCell cell)
+    {
+        if (this.cell != null) {
+            this.cell.tile = null;
+        }
+
+        this.cell = cell;
+        this.cell.tile = this;
+
+        StartCoroutine(Animate(cell.transform.position, false));
+    }
+    
+    public void Merge(TileCell cell)
+    {
+        if (this.cell != null) {
+            this.cell.tile = null;
+        }
+
+        this.cell = null;
+        cell.tile.locked = true;
+
+        StartCoroutine(Animate(cell.transform.position, true));
+    }
+
+    private IEnumerator Animate(Vector3 to, bool merging)
+    {
+        float elapsed = 0f;
+        float duration = 0.1f;
+
+        Vector3 from = transform.position;
+
+        while (elapsed < duration)
+        {
+            transform.position = Vector3.Lerp(from, to, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = to;
+
+        if (merging) {
+            Destroy(gameObject);
+        }
+    }
 }
